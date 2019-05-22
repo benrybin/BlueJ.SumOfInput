8943bdaaa3e134abcf9ef2c5486417f6c5361512
Ben Rybinski <benrybinski@zipcoders-MacBook-Pro.local>
2019-05-22 15:47:08 -0400

    Yes

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        int counter= 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for(int i=1; i<= N; i++){
+            counter = i + counter;
+            
+        
 
     }
+    System.out.println(counter);
+}
 }
